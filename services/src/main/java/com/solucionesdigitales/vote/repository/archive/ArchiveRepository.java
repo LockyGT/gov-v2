e25b695c992da0357112f779a68232cde2dcc14c
suuperjeivier <jbritopc@gmail.com>
2019-05-13 13:01:53 -0500

    SE BORRA CLASE DE MAS

--- services/src/main/java/com/solucionesdigitales/vote/repository/archive/ArchiveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.solucionesdigitales.vote.repository.archive;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.solucionesdigitales.vote.entity.archive.Archive;
-
-public interface ArchiveRepository extends MongoRepository<Archive, String> {
-
-}
