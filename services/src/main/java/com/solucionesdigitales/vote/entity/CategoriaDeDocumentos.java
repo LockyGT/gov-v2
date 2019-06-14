d14195bf5689bbf3ba13081b55b8358ec16ea2b3
jorgeluisrosalessanchez <jorge.rosales@maritz.com>
2019-06-13 23:42:10 -0500

    adding first commit for new feature

---
+++ services/src/main/java/com/solucionesdigitales/vote/entity/CategoriaDeDocumentos.java
@@ -0,0 +1,8 @@
+package com.solucionesdigitales.vote.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "categoria-documentos")
+public class CategoriaDeDocumentos {
+
+}
